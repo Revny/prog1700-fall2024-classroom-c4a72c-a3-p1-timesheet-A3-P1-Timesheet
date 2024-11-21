b0f546564ae5b007937ad58a0b40aeab4c199310
Revny <Lsilver205@gmail.com>
2024-11-21 09:54:49 -0400

    done

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -4,18 +4,36 @@
 #               information calculated about those entries as output. 
 
 #Student #:     
-#Student Name:  
+#Student Name:Lucas  
+
+
+    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    
+workday =[1,2,3,4,5] #the lists that the program uses
+hoursworked = []
+hours =[]
+
+
+for i in range(len(workday)):
+        hours=int(input(f"enter in the hours you worked #{i+1}:")) #i will increse by one untill it hits 5
+        hoursworked.append(hours)
+        
+
+totalhours =sum (hoursworked) #totalhours and averagehours
+average = totalhours/5
+
+print(f"the total hours within in the week is {totalhours}")
+(print(f"the average hours are {average}"))
+
+minworkhours= min(hoursworked) #min hours
+print(f"your min work hours are {minworkhours}")
+
+
+           
+
+
+   # YOUR CODE ENDS HERE
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
-
-    # YOUR CODE ENDS HERE
-
-main()
+    
+    main()
